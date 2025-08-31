9b4edc9a20c3e3ede54fbadfbf10fc012ad3ee57
Peter Carney <pc@petercarney.com>
2025-08-31 09:27:45 -0700

    fix asset files

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+$(function(){
+	
+	// Checking for CSS 3D transformation support
+	$.support.css3d = supportsCSS3D();
+	
+	var formContainer = $('#formContainer');
+	
+	// Listening for clicks on the ribbon links
+	$('.flipLink').click(function(e){
+		
+		// Flipping the forms
+		formContainer.toggleClass('flipped');
+		
+		// If there is no CSS3 3D support, simply
+		// hide the login form (exposing the recover one)
+		if(!$.support.css3d){
+			$('#login').toggle();
+		}
+		e.preventDefault();
+	});
+	
+	$("#submit").click(function(e){
+		// Preventing form submissions. If you implement
+		// a backend, you might want to remove this code
+		//e.preventDefault();
+		if($("#pcnm").val()==''){
+			alert("Please enter your name");
+			$("#pcnm").focus();
+			return false;
+		}
+		if($("#pcem").val()==''){
+			alert("Please enter your email address");
+			$("#pcem").focus();
+			return false;
+		}
+		if($("#pcmg").val()==''){
+			alert("Please enter a message");
+			$("#pcmg").focus();
+			return false;
+		}
+		$("#send").submit();
+	});
+	
+	
+	// A helper function that checks for the 
+	// support of the 3D CSS3 transformations.
+	function supportsCSS3D() {
+		var props = [
+			'perspectiveProperty', 'WebkitPerspective', 'MozPerspective'
+		], testDom = document.createElement('a');
+		  
+		for(var i=0; i&lt;props.length; i++){
+			if(props[i] in testDom.style){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+});
